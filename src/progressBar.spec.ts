3b351ac0b599005395ddfa8abe822c1984d07829
Homa Wong <homawong@gmail.com>
2018-11-28 14:35:01 -0800

    feat: add value and length support

--- src/progressBar.spec.ts
+++ src/progressBar.spec.ts
@@ -1,7 +1,23 @@
 import t from 'assert';
 import { progressBar } from '.';
 
-test('return empty string', () => {
-  const actual = progressBar()
-  t.strictEqual(actual, '')
+
+test('prints 0.5 as 50.0%', () => {
+  const actual = progressBar(0.5)
+  t.strictEqual(actual, '[-----------|----------] 50.0%')
+})
+
+test('prints 0 as 0.0%', () => {
+  const actual = progressBar(0)
+  t.strictEqual(actual, '[|----------------------] 0.0%')
+})
+
+test('default length is 30', () => {
+  const actual = progressBar(0.5)
+  t.strictEqual(actual.length, 30)
+})
+
+test('options.length control the length of the overall result', () => {
+  const actual = progressBar(0.5, { length: 15 })
+  t.strictEqual(actual.length, 15)
 })
--- src/progressBar.ts
+++ src/progressBar.ts
@@ -1,3 +1,18 @@
-export function progressBar() {
-  return ''
+export function progressBar(currentValue: number, options: { length: number } = { length: 30 }) {
+  const percent = toPercentage(currentValue, 1)
+  const spaceLength = 1
+  return `${toBar(currentValue, options.length - percent.length - spaceLength)} ${percent}`
+}
+
+function toBar(percent: number, length: number) {
+  const bracketLength = 1
+  const barInsideLength = length - bracketLength * 2
+  const location = Math.round(percent * barInsideLength)
+  const bar = new Array(barInsideLength).fill('-')
+  bar[location] = '|'
+  return `[${bar.join('')}]`
+}
+
+function toPercentage(value: number, fractionDigits: number) {
+  return `${(value * 100).toFixed(fractionDigits)}%`
 }
