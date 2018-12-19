b5ca0a1a5510ec98a53f7b8d60cdd3bca4efd5ab
Homa Wong (unional) <homawong@gmail.com>
2018-12-19 13:40:29 -0800

    fix node6 ...

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 const common = require('@unional/devpkg-node/simple/config/jest.common')
-module.exports = {
-  ...common,
+module.exports = Object.assign(common, {
   'testEnvironment': 'node'
-}
+})
