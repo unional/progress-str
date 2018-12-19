100cfe1f9da8d4ef189aa18d8d5590e49637676c
Homa Wong (unional) <homawong@gmail.com>
2018-12-19 13:16:36 -0800

    feat: convert to classless and implement maxValue

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,13 @@
 import { BaseError } from 'make-error'
 
-export class CurrentValueOutOfRange extends BaseError {
+export class CurrentValueOutOfBound extends BaseError {
   constructor(public value: number) {
     super(`The current value (${value}) must be between 0 and 1`)
+  }
+}
+
+export class MaxValueOutOfBound extends BaseError {
+  constructor(public value: number) {
+    super(`The max value (${value}) must be greater than 0`)
   }
 }
--- src/progressBar.spec.ts
+++ src/progressBar.spec.ts
@@ -1,54 +1,63 @@
 import t from 'assert';
 import a from 'assertron';
-import { CurrentValueOutOfRange, ProgressBar } from '.';
+import { CurrentValueOutOfBound, progressBar, MaxValueOutOfBound } from '.';
 
 
 test('prints 0.5 as 50.0%', () => {
-  const bar = new ProgressBar()
+  const bar = progressBar()
   const actual = bar.render(0.5)
   t.strictEqual(actual, '[-----------|----------] 50.0%')
 })
 
 test('prints 0 as 0.0%', () => {
-  const bar = new ProgressBar()
+  const bar = progressBar()
   const actual = bar.render(0)
   t.strictEqual(actual, '[|----------------------] 0.0%')
 })
 
 test('options can be empty', () => {
   // tslint:disable-next-line
-  new ProgressBar({})
+  progressBar({})
 })
 
 test('default length is 30', () => {
-  const bar = new ProgressBar()
+  const bar = progressBar()
   const actual = bar.render(0.5)
   t.strictEqual(actual.length, 30)
 })
 
 test('options.length control the length of the overall result', () => {
-  const bar = new ProgressBar({ length: 15 })
+  const bar = progressBar({ length: 15 })
   const actual = bar.render(0.5)
   t.strictEqual(actual.length, 15)
 })
 
 test.skip('options.length less than X will throw LengthTooShort', () => { })
 
-test('current value > 1 will throw CurrentValueOutOfRange', () => {
-  const bar = new ProgressBar()
-  a.throws(() => bar.render(2), CurrentValueOutOfRange)
+test('current value > 1 will throw CurrentValueOutOfBound', () => {
+  const bar = progressBar()
+  a.throws(() => bar.render(2), CurrentValueOutOfBound)
 })
 
 test('current value === 1 is treated as 100%', () => {
-  const bar = new ProgressBar()
+  const bar = progressBar()
   const actual = bar.render(1)
   t.strictEqual(actual, '[---------------------|] 100.0%')
 })
 
-test.skip('specify max value', () => {
-  const bar = new ProgressBar({ maxValue: 20 })
+test('can specify max value', () => {
+  const bar = progressBar({ maxValue: 20 })
   const actual = bar.render(10)
-    t.strictEqual(actual, '[-----------|----------] 50.0%')
+  t.strictEqual(actual, '[-----------|----------] 50.0%')
+})
+
+test('currentValue > specified maxValue throws CurrentValueOutOfBound', () => {
+  const bar = progressBar({ maxValue: 10 })
+  a.throws(() => bar.render(12), CurrentValueOutOfBound)
+})
+
+test('maxValue = 0 throws MaxValueOutOfBound', () => {
+  a.throws(() => progressBar({ maxValue: 0 }), MaxValueOutOfBound)
 })
 
 test.skip('can spec max', () => {
