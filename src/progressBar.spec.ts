03fc746bd3e79eb1314afcf2d4d3d0726704f29c
Homa Wong (unional) <homawong@gmail.com>
2018-12-26 14:43:08 -0800

    feat: add twoValuesProgressBar()

--- src/progressBar.spec.ts
+++ src/progressBar.spec.ts
@@ -1,6 +1,6 @@
 import t from 'assert';
 import a from 'assertron';
-import { CurrentValueOutOfBound, progressBar, MaxValueOutOfBound } from '.';
+import { CurrentValueOutOfBound, progressBar, MaxValueOutOfBound, twoValuesProgressBar } from '.';
 
 
 test('prints 0.5 as 50.0%', () => {
@@ -60,7 +60,35 @@ test('maxValue = 0 throws MaxValueOutOfBound', () => {
   a.throws(() => progressBar({ maxValue: 0 }), MaxValueOutOfBound)
 })
 
-test.skip('can spec max', () => {
+test('two values', () => {
+  const bar = twoValuesProgressBar()
+  const actual = bar.render(0.5, 1)
+  t.strictEqual(actual, '[--------|------|] 50.0% 100.0%')
+})
+
+test('two values', () => {
+  const bar = twoValuesProgressBar()
+  const actual = bar.render(1, 0.5)
+  t.strictEqual(actual, '[--------|------|] 100.0% 50.0%')
+})
+
+test('two same values', () => {
+  const bar = twoValuesProgressBar()
+  const actual = bar.render(0.5, 0.5)
+  t.strictEqual(actual, '[--------|-------] 50.0% 50.0%')
+})
+
+test('first value > specified maxValue throws CurrentValueOutOfBound', () => {
+  const bar = twoValuesProgressBar({ maxValue: 10 })
+  a.throws(() => bar.render(12, 1), CurrentValueOutOfBound)
+})
+
+test('second value > specified maxValue throws CurrentValueOutOfBound', () => {
+  const bar = twoValuesProgressBar({ maxValue: 10 })
+  a.throws(() => bar.render(1, 12), CurrentValueOutOfBound)
+})
+
+test.skip('formats', () => {
   // 10 [-----|-----] 20
   // [-----|-----] 10/20
   // [-----|-----] 50%
--- src/progressBar.ts
+++ src/progressBar.ts
@@ -11,26 +11,38 @@ export function progressBar(options?: Partial<Options>) {
   if (actualOptions.maxValue <= 0) throw new MaxValueOutOfBound(actualOptions.maxValue)
   return {
     render(value: number) {
-      return progressBarPercent(value, actualOptions)
+      return progressBarPercent(actualOptions, value)
     }
   }
 }
 
-function progressBarPercent(currentValue: number, options: Options) {
-  if (currentValue > options.maxValue) throw new CurrentValueOutOfBound(currentValue)
-
-  const percentage = currentValue / options.maxValue
-  const percentStr = toStringPercentage(percentage, 1)
-  const spaceLength = 1
-  return `${toBar(percentage, options.length - percentStr.length - spaceLength)} ${percentStr}`
+export function twoValuesProgressBar(options?: Partial<Options>) {
+  const actualOptions = unpartial({ length: 30, maxValue: 1 }, options)
+  if (actualOptions.maxValue <= 0) throw new MaxValueOutOfBound(actualOptions.maxValue)
+  return {
+    render(value1: number, value2: number) {
+      return progressBarPercent(actualOptions, value1, value2)
+    }
+  }
 }
 
-function toBar(percent: number, length: number) {
+
+function progressBarPercent(options: Options, ...values: number[]) {
+  values.forEach(v => { if (v > options.maxValue) throw new CurrentValueOutOfBound(v) })
+
+  const percentages = values.map(v => v / options.maxValue)
+  const percentStr = percentages.map(p => toStringPercentage(p, 1)).join(' ')
+
+  const spaceLength = 1
+  return `${toBar(percentages, options.length - percentStr.length - spaceLength)} ${percentStr}`
+}
+
+function toBar(percentages: number[], length: number) {
   const bracketLength = 1
   const barInsideLength = length - bracketLength * 2
-  const location = Math.round(percent * barInsideLength)
+  const locations = percentages.map(p => Math.round(p * barInsideLength))
   const bar = new Array(barInsideLength).fill('-')
-  bar[location] = '|'
+  locations.forEach(l => bar[l] = '|')
   return `[${bar.join('')}]`
 }
 
