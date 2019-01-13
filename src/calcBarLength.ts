9c7b2738ac5ddc2e138c0a21cd2a8815cf8a1bc8
Homa Wong (unional) <homawong@gmail.com>
2019-01-12 21:11:51 -0800

    fix: render error message instead of throwing during render.

--- src/calcBarLength.ts
+++ src/calcBarLength.ts
@@ -1,5 +1,14 @@
+import stringLength from 'string-length';
+import { BaseOptions } from './interfaces';
+import { renderText } from './renderText';
+import { ValueEntry } from './ValueEntry';
 
 export function calcBarLength(maxLength: number, textLength: number) {
   const spaceLength = 1
   return maxLength - textLength - spaceLength
+}
+
+export function calcBarLengthForEntries(baseOption: BaseOptions, entries: ValueEntry[]) {
+  const text = renderText(baseOption, entries)
+  return calcBarLength(baseOption.length, stringLength(text))
 }
--- src/validate.ts
+++ src/validate.ts
@@ -1,8 +1,8 @@
 import stringLength from 'string-length';
-import { calcBarLength } from './calcBarLength';
+import { calcBarLengthForEntries } from './calcBarLength';
 import { InvalidMarker, LengthTooShort, MaxValueOutOfBound } from './errors';
 import { BarFormat, BaseOptions, ValueOptions } from './interfaces';
-import { renderText } from './renderText';
+import { ValueEntry } from './ValueEntry';
 
 export function validateValueOptions(options: Partial<ValueOptions>) {
   if (options.max !== undefined && (options.max <= 0)) throw new MaxValueOutOfBound(options.max)
@@ -14,9 +14,8 @@ export function validateBarFormat(format: Partial<BarFormat>) {
   if (format.completedMarker !== undefined && stringLength(format.completedMarker) !== 1) throw new InvalidMarker(format.completedMarker)
 }
 
-export function validateLength(baseOption: BaseOptions, valueOptions: ValueOptions[]) {
-  const entries = valueOptions.map(o => ({ value: o.max, ...o }))
-  const text = renderText(baseOption, entries)
-  const len = calcBarLength(baseOption.length, stringLength(text))
-  if (len < 5) throw new LengthTooShort(baseOption.length, entries.length)
+export function validateLength(baseOption: BaseOptions, entries: ValueEntry[]) {
+  const len = calcBarLengthForEntries(baseOption, entries)
+  if (len < 3 + stringLength(baseOption.bar.leftBracketMarker) + stringLength(baseOption.bar.rightBracketMarker))
+    throw new LengthTooShort(baseOption.length, entries.length)
 }
